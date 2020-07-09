7105ab2f6f0163e3c78e35398f9a1e34698b7514
nyasharonaldo <nyasharonaldo@gmail.com>
2020-07-09 22:32:33 +0100

    SCSS and About page markdown. Still cms to do

--- src/pages/about/index.md
+++ src/pages/about/index.md
@@ -2,5 +2,18 @@
 templateKey: about
 aboutPage:
   title: Do some work
-  img10: ../../img/img-10.jpg
+  description: >-
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  contactSlogan: Get in touch
+  sections:
+    - title: Incididunt ut labore 
+      body: >-
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luctus at sem quis varius. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras ultrices hendrerit nisl.
+    - title: Phasellus iaculis magna
+      body: >-
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio exercitationem cum sunt cupiditate quisquam debitis voluptas perspiciatis incidunt esse alias, dolore quos assumenda! Hic provident autem vel dicta sit sapiente.
+    - title: Class aptent
+      body: >-
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates quis, nihil incidunt doloremque soluta obcaecati earum, dolorum quia explicabo eum rerum. Quasi repellat ea eligendi cum sit nobis eos voluptate laudantium nisi? Dolore, similique voluptas!
+  image: ../../img/img-10.jpg
 ---
--- src/templates/about.js
+++ src/templates/about.js
@@ -4,8 +4,13 @@ import { graphql, Link } from "gatsby"
 import Img from "gatsby-image"
 
 const about = ({ data }) => {
-  const { img10 } = data.markdownRemark.frontmatter.aboutPage
-  console.log(data)
+  const {
+    title,
+    description,
+    sections,
+    image,
+  } = data.markdownRemark.frontmatter.aboutPage
+
   return (
     <Layout current="about">
       <main className="" id="main-collapse">
@@ -14,45 +19,22 @@ const about = ({ data }) => {
             <Img
               className="img-responsive"
               alt=""
-              fluid={img10.childImageSharp.fluid}
+              fluid={image.childImageSharp.fluid}
             />
           </div>
           <div className="col-xs-12 col-md-6">
-            <h1>About me</h1>
+            <h1>{title}</h1>
 
-            <p>
-              Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia deserunt mollit
-              anim id est laborum.
-            </p>
+            <p>{description}</p>
 
-            <h3>Incididunt ut labore </h3>
-
-            <p>
-              Class aptent taciti sociosqu ad litora torquent per conubia
-              nostra, per inceptos himenaeos. Phasellus iaculis magna sagittis
-              elit sagittis, at hendrerit lorem venenatis. Morbi accumsan
-              iaculis blandit. Cras ultrices hendrerit nisl.
-            </p>
-
-            <h3>Ut enim ad minim veniam </h3>
-
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              luctus at sem quis varius. Class aptent taciti sociosqu ad litora
-              torquent per conubia nostra, per inceptos himenaeos. Phasellus
-              iaculis magna sagittis elit sagittis, at hendrerit lorem
-              venenatis. Morbi accumsan iaculis blandit. Cras ultrices hendrerit
-              nisl.
-            </p>
-            <h3>Phasellus iaculis magna </h3>
-            <p>
-              Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia deserunt mollit
-              anim id est laborum.
-            </p>
+            {sections.map(section => {
+              return (
+                <div key={section.title}>
+                  <h3>{section.title}</h3>
+                  <p>{section.body}</p>
+                </div>
+              )
+            })}
 
             <Link to="/contact" className="btn btn-primary" title="">
               {" "}
@@ -72,7 +54,14 @@ export const pageQuery = graphql`
     markdownRemark(id: { eq: $id }) {
       frontmatter {
         aboutPage {
-          img10 {
+          title
+          description
+          contactSlogan
+          sections{
+            title
+            body
+          }
+          image {
             childImageSharp {
               fluid(maxWidth: 10000, quality: 100) {
                 ...GatsbyImageSharpFluid
